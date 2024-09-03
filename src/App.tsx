54d70faf1f326a3b4ee179161ff97c7b97f838ac
darius molotokas <dariusm.it12@gmail.com>
2024-09-03 09:48:33 +0300

    Base logic done, game is working

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 
 interface type {
   id: number;
@@ -19,8 +19,224 @@ function App() {
 
   let [box, changeBox] = useState(initialData());
 
+  //Winning Logic
+  useEffect(() => {
+    //X winning logic
+    if (box[0].mark === "X" && box[1].mark === "X" && box[2].mark === "X") {
+      console.log("X won! Game starts again.");
+      changeBox(
+        box.map((prev) => {
+          return { ...prev, mark: "", isClicked: false };
+        })
+      );
+      setMark("X");
+    } else if (
+      box[3].mark === "X" &&
+      box[4].mark === "X" &&
+      box[5].mark === "X"
+    ) {
+      console.log("X won! Game starts again.");
+      changeBox(
+        box.map((prev) => {
+          return { ...prev, mark: "", isClicked: false };
+        })
+      );
+      setMark("X");
+    } else if (
+      box[6].mark === "X" &&
+      box[7].mark === "X" &&
+      box[8].mark === "X"
+    ) {
+      console.log("X won! Game starts again.");
+      changeBox(
+        box.map((prev) => {
+          return { ...prev, mark: "", isClicked: false };
+        })
+      );
+      setMark("X");
+    } else if (
+      box[0].mark === "X" &&
+      box[3].mark === "X" &&
+      box[6].mark === "X"
+    ) {
+      console.log("X won! Game starts again.");
+      changeBox(
+        box.map((prev) => {
+          return { ...prev, mark: "", isClicked: false };
+        })
+      );
+      setMark("X");
+    } else if (
+      box[1].mark === "X" &&
+      box[4].mark === "X" &&
+      box[7].mark === "X"
+    ) {
+      console.log("X won! Game starts again.");
+      changeBox(
+        box.map((prev) => {
+          return { ...prev, mark: "", isClicked: false };
+        })
+      );
+      setMark("X");
+    } else if (
+      box[2].mark === "X" &&
+      box[5].mark === "X" &&
+      box[8].mark === "X"
+    ) {
+      console.log("X won! Game starts again.");
+      changeBox(
+        box.map((prev) => {
+          return { ...prev, mark: "", isClicked: false };
+        })
+      );
+      setMark("X");
+    } else if (
+      box[0].mark === "X" &&
+      box[4].mark === "X" &&
+      box[8].mark === "X"
+    ) {
+      console.log("X won! Game starts again.");
+      changeBox(
+        box.map((prev) => {
+          return { ...prev, mark: "", isClicked: false };
+        })
+      );
+      setMark("X");
+    } else if (
+      box[6].mark === "X" &&
+      box[4].mark === "X" &&
+      box[2].mark === "X"
+    ) {
+      console.log("X won! Game starts again.");
+      changeBox(
+        box.map((prev) => {
+          return { ...prev, mark: "", isClicked: false };
+        })
+      );
+      setMark("X");
+    }
+    //O winning logic
+    else if (
+      box[0].mark === "O" &&
+      box[1].mark === "O" &&
+      box[2].mark === "O"
+    ) {
+      console.log("O won! Game starts again.");
+      changeBox(
+        box.map((prev) => {
+          return { ...prev, mark: "", isClicked: false };
+        })
+      );
+      setMark("X");
+    } else if (
+      box[3].mark === "O" &&
+      box[4].mark === "O" &&
+      box[5].mark === "O"
+    ) {
+      console.log("O won! Game starts again.");
+      changeBox(
+        box.map((prev) => {
+          return { ...prev, mark: "", isClicked: false };
+        })
+      );
+      setMark("X");
+    } else if (
+      box[6].mark === "O" &&
+      box[7].mark === "O" &&
+      box[8].mark === "O"
+    ) {
+      console.log("O won! Game starts again.");
+      changeBox(
+        box.map((prev) => {
+          return { ...prev, mark: "", isClicked: false };
+        })
+      );
+      setMark("X");
+    } else if (
+      box[0].mark === "O" &&
+      box[3].mark === "O" &&
+      box[6].mark === "O"
+    ) {
+      console.log("O won! Game starts again.");
+      changeBox(
+        box.map((prev) => {
+          return { ...prev, mark: "", isClicked: false };
+        })
+      );
+      setMark("X");
+    } else if (
+      box[1].mark === "O" &&
+      box[4].mark === "O" &&
+      box[7].mark === "O"
+    ) {
+      console.log("O won! Game starts again.");
+      changeBox(
+        box.map((prev) => {
+          return { ...prev, mark: "", isClicked: false };
+        })
+      );
+      setMark("X");
+    } else if (
+      box[2].mark === "O" &&
+      box[5].mark === "O" &&
+      box[8].mark === "O"
+    ) {
+      console.log("O won! Game starts again.");
+      changeBox(
+        box.map((prev) => {
+          return { ...prev, mark: "", isClicked: false };
+        })
+      );
+      setMark("X");
+    } else if (
+      box[0].mark === "O" &&
+      box[4].mark === "O" &&
+      box[8].mark === "O"
+    ) {
+      console.log("O won! Game starts again.");
+      changeBox(
+        box.map((prev) => {
+          return { ...prev, mark: "", isClicked: false };
+        })
+      );
+      setMark("X");
+    } else if (
+      box[6].mark === "O" &&
+      box[4].mark === "O" &&
+      box[2].mark === "O"
+    ) {
+      console.log("O won! Game starts again.");
+      changeBox(
+        box.map((prev) => {
+          return { ...prev, mark: "", isClicked: false };
+        })
+      );
+      setMark("X");
+    } else {
+      //A draw case. If no winners emerged.
+      if (
+        box[0].isClicked === true &&
+        box[1].isClicked === true &&
+        box[2].isClicked === true &&
+        box[3].isClicked === true &&
+        box[4].isClicked === true &&
+        box[5].isClicked === true &&
+        box[6].isClicked === true &&
+        box[7].isClicked === true &&
+        box[8].isClicked === true
+      ) {
+        console.log("A draw! Game starts again.");
+        changeBox(
+          box.map((prev) => {
+            return { ...prev, mark: "", isClicked: false };
+          })
+        );
+        setMark("X");
+      }
+    }
+  }, [box]);
+
   function handleClick(id: number) {
-    console.log("Item with the id of " + id + " was clicked!");
     changeBox(
       box.map((prev) => {
         if (prev.id === id) {
@@ -57,9 +273,6 @@ function App() {
           );
         })}
       </div>
-      <button onClick={() => console.log(JSON.stringify(box, undefined, 2))}>
-        Test state values
-      </button>
     </div>
   );
 }
