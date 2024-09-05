46878e0b5b61d33828c9771530b76d71475d4341
darius molotokas <dariusm.it12@gmail.com>
2024-09-05 20:57:04 +0300

    CPU move logic seems to be working

--- src/App.tsx
+++ src/App.tsx
@@ -6,6 +6,8 @@ interface type {
   isClicked: boolean;
 }
 
+let PlayerClickID = 0;
+
 function App() {
   let data: type[] = [];
   let [mark, setMark] = useState("X");
@@ -17,7 +19,7 @@ function App() {
     return data;
   }
 
-  let [box, changeBox] = useState(initialData);
+  let [board, changeBoard] = useState(initialData);
 
   let [xCounter, setXCounter] = useState(0);
   let [oCounter, setOCounter] = useState(0);
@@ -26,101 +28,112 @@ function App() {
   //Winning Logic
   useEffect(() => {
     //X winning logic
-    if (box[0].mark === "X" && box[1].mark === "X" && box[2].mark === "X") {
+    if (
+      board[0].mark === "X" &&
+      board[1].mark === "X" &&
+      board[2].mark === "X"
+    ) {
       console.log("X won! Game starts again.");
-      changeBox(
-        box.map((prev) => {
+      changeBoard(
+        board.map((prev) => {
           return { ...prev, mark: "", isClicked: false };
         })
       );
       setMark("X");
       setXCounter((prev) => prev + 1);
-    } else if (
-      box[3].mark === "X" &&
-      box[4].mark === "X" &&
-      box[5].mark === "X"
+    }
+    if (
+      board[3].mark === "X" &&
+      board[4].mark === "X" &&
+      board[5].mark === "X"
     ) {
       console.log("X won! Game starts again.");
-      changeBox(
-        box.map((prev) => {
+      changeBoard(
+        board.map((prev) => {
           return { ...prev, mark: "", isClicked: false };
         })
       );
       setMark("X");
       setXCounter((prev) => prev + 1);
-    } else if (
-      box[6].mark === "X" &&
-      box[7].mark === "X" &&
-      box[8].mark === "X"
+    }
+    if (
+      board[6].mark === "X" &&
+      board[7].mark === "X" &&
+      board[8].mark === "X"
     ) {
       console.log("X won! Game starts again.");
-      changeBox(
-        box.map((prev) => {
+      changeBoard(
+        board.map((prev) => {
           return { ...prev, mark: "", isClicked: false };
         })
       );
       setMark("X");
       setXCounter((prev) => prev + 1);
-    } else if (
-      box[0].mark === "X" &&
-      box[3].mark === "X" &&
-      box[6].mark === "X"
+    }
+    if (
+      board[0].mark === "X" &&
+      board[3].mark === "X" &&
+      board[6].mark === "X"
     ) {
       console.log("X won! Game starts again.");
-      changeBox(
-        box.map((prev) => {
+      changeBoard(
+        board.map((prev) => {
           return { ...prev, mark: "", isClicked: false };
         })
       );
       setMark("X");
       setXCounter((prev) => prev + 1);
-    } else if (
-      box[1].mark === "X" &&
-      box[4].mark === "X" &&
-      box[7].mark === "X"
+    }
+    if (
+      board[1].mark === "X" &&
+      board[4].mark === "X" &&
+      board[7].mark === "X"
     ) {
       console.log("X won! Game starts again.");
-      changeBox(
-        box.map((prev) => {
+      changeBoard(
+        board.map((prev) => {
           return { ...prev, mark: "", isClicked: false };
         })
       );
       setMark("X");
       setXCounter((prev) => prev + 1);
-    } else if (
-      box[2].mark === "X" &&
-      box[5].mark === "X" &&
-      box[8].mark === "X"
+    }
+    if (
+      board[2].mark === "X" &&
+      board[5].mark === "X" &&
+      board[8].mark === "X"
     ) {
       console.log("X won! Game starts again.");
-      changeBox(
-        box.map((prev) => {
+      changeBoard(
+        board.map((prev) => {
           return { ...prev, mark: "", isClicked: false };
         })
       );
       setMark("X");
       setXCounter((prev) => prev + 1);
-    } else if (
-      box[0].mark === "X" &&
-      box[4].mark === "X" &&
-      box[8].mark === "X"
+    }
+    if (
+      board[0].mark === "X" &&
+      board[4].mark === "X" &&
+      board[8].mark === "X"
     ) {
       console.log("X won! Game starts again.");
-      changeBox(
-        box.map((prev) => {
+      changeBoard(
+        board.map((prev) => {
           return { ...prev, mark: "", isClicked: false };
         })
       );
       setMark("X");
       setXCounter((prev) => prev + 1);
-    } else if (
-      box[6].mark === "X" &&
-      box[4].mark === "X" &&
-      box[2].mark === "X"
+    }
+    if (
+      board[6].mark === "X" &&
+      board[4].mark === "X" &&
+      board[2].mark === "X"
     ) {
       console.log("X won! Game starts again.");
-      changeBox(
-        box.map((prev) => {
+      changeBoard(
+        board.map((prev) => {
           return { ...prev, mark: "", isClicked: false };
         })
       );
@@ -128,105 +141,112 @@ function App() {
       setXCounter((prev) => prev + 1);
     }
     //O winning logic
-    else if (
-      box[0].mark === "O" &&
-      box[1].mark === "O" &&
-      box[2].mark === "O"
+    if (
+      board[0].mark === "O" &&
+      board[1].mark === "O" &&
+      board[2].mark === "O"
     ) {
       console.log("O won! Game starts again.");
-      changeBox(
-        box.map((prev) => {
+      changeBoard(
+        board.map((prev) => {
           return { ...prev, mark: "", isClicked: false };
         })
       );
       setMark("X");
       setOCounter((prev) => prev + 1);
-    } else if (
-      box[3].mark === "O" &&
-      box[4].mark === "O" &&
-      box[5].mark === "O"
+    }
+    if (
+      board[3].mark === "O" &&
+      board[4].mark === "O" &&
+      board[5].mark === "O"
     ) {
       console.log("O won! Game starts again.");
-      changeBox(
-        box.map((prev) => {
+      changeBoard(
+        board.map((prev) => {
           return { ...prev, mark: "", isClicked: false };
         })
       );
       setMark("X");
       setOCounter((prev) => prev + 1);
-    } else if (
-      box[6].mark === "O" &&
-      box[7].mark === "O" &&
-      box[8].mark === "O"
+    }
+    if (
+      board[6].mark === "O" &&
+      board[7].mark === "O" &&
+      board[8].mark === "O"
     ) {
       console.log("O won! Game starts again.");
-      changeBox(
-        box.map((prev) => {
+      changeBoard(
+        board.map((prev) => {
           return { ...prev, mark: "", isClicked: false };
         })
       );
       setMark("X");
       setOCounter((prev) => prev + 1);
-    } else if (
-      box[0].mark === "O" &&
-      box[3].mark === "O" &&
-      box[6].mark === "O"
+    }
+    if (
+      board[0].mark === "O" &&
+      board[3].mark === "O" &&
+      board[6].mark === "O"
     ) {
       console.log("O won! Game starts again.");
-      changeBox(
-        box.map((prev) => {
+      changeBoard(
+        board.map((prev) => {
           return { ...prev, mark: "", isClicked: false };
         })
       );
       setMark("X");
       setOCounter((prev) => prev + 1);
-    } else if (
-      box[1].mark === "O" &&
-      box[4].mark === "O" &&
-      box[7].mark === "O"
+    }
+    if (
+      board[1].mark === "O" &&
+      board[4].mark === "O" &&
+      board[7].mark === "O"
     ) {
       console.log("O won! Game starts again.");
-      changeBox(
-        box.map((prev) => {
+      changeBoard(
+        board.map((prev) => {
           return { ...prev, mark: "", isClicked: false };
         })
       );
       setMark("X");
       setOCounter((prev) => prev + 1);
-    } else if (
-      box[2].mark === "O" &&
-      box[5].mark === "O" &&
-      box[8].mark === "O"
+    }
+    if (
+      board[2].mark === "O" &&
+      board[5].mark === "O" &&
+      board[8].mark === "O"
     ) {
       console.log("O won! Game starts again.");
-      changeBox(
-        box.map((prev) => {
+      changeBoard(
+        board.map((prev) => {
           return { ...prev, mark: "", isClicked: false };
         })
       );
       setMark("X");
       setOCounter((prev) => prev + 1);
-    } else if (
-      box[0].mark === "O" &&
-      box[4].mark === "O" &&
-      box[8].mark === "O"
+    }
+    if (
+      board[0].mark === "O" &&
+      board[4].mark === "O" &&
+      board[8].mark === "O"
     ) {
       console.log("O won! Game starts again.");
-      changeBox(
-        box.map((prev) => {
+      changeBoard(
+        board.map((prev) => {
           return { ...prev, mark: "", isClicked: false };
         })
       );
       setMark("X");
       setOCounter((prev) => prev + 1);
-    } else if (
-      box[6].mark === "O" &&
-      box[4].mark === "O" &&
-      box[2].mark === "O"
+    }
+    if (
+      board[6].mark === "O" &&
+      board[4].mark === "O" &&
+      board[2].mark === "O"
     ) {
       console.log("O won! Game starts again.");
-      changeBox(
-        box.map((prev) => {
+      changeBoard(
+        board.map((prev) => {
           return { ...prev, mark: "", isClicked: false };
         })
       );
@@ -235,19 +255,19 @@ function App() {
     } else {
       //A draw case. If no winners emerged.
       if (
-        box[0].isClicked === true &&
-        box[1].isClicked === true &&
-        box[2].isClicked === true &&
-        box[3].isClicked === true &&
-        box[4].isClicked === true &&
-        box[5].isClicked === true &&
-        box[6].isClicked === true &&
-        box[7].isClicked === true &&
-        box[8].isClicked === true
+        board[0].isClicked === true &&
+        board[1].isClicked === true &&
+        board[2].isClicked === true &&
+        board[3].isClicked === true &&
+        board[4].isClicked === true &&
+        board[5].isClicked === true &&
+        board[6].isClicked === true &&
+        board[7].isClicked === true &&
+        board[8].isClicked === true
       ) {
         console.log("A Tie! Game starts again.");
-        changeBox(
-          box.map((prev) => {
+        changeBoard(
+          board.map((prev) => {
             return { ...prev, mark: "", isClicked: false };
           })
         );
@@ -255,14 +275,20 @@ function App() {
         setTieCounter((prev) => prev + 1);
       }
     }
-  }, [box]);
+  }, [board]);
 
   let [gameType, setGameType] = useState("solo");
+  let [playerMove, registerPlayerMove] = useState(0);
+  let randomNumber = 0;
+
+  function getPlayerClickID(id: number) {
+    PlayerClickID = id;
+  }
 
   function handleClick(id: number) {
     if (gameType === "solo") {
-      changeBox(
-        box.map((prev) => {
+      changeBoard(
+        board.map((prev) => {
           if (prev.id === id) {
             if (prev.isClicked === false) {
               if (mark === "X") {
@@ -279,51 +305,107 @@ function App() {
           return prev;
         })
       );
+      //Player move on click when versus CPU is selected
     } else if (gameType === "cpu") {
-      changeBox(
-        box.map((prev) => {
-          if (prev.id === id) {
-            if (prev.isClicked === false) {
-              if (mark === "X") {
-                setMark("O");
-                return { ...prev, mark: "X", isClicked: true };
-              }
+      if (mark === "X") {
+        getPlayerClickID(id);
+        changeBoard(
+          board.map((prev) => {
+            console.log("Player clicked at id of " + id);
+            if (prev.id === id && prev.isClicked === false) {
+              registerPlayerMove((prev) => (prev += 1));
+              setMark("O");
+              return { ...prev, mark: "X", isClicked: true };
             }
             return prev;
-          }
-          return prev;
-        })
-      );
-    }
-  }
-
-  function getRandomInt(number: number) {
-    return Math.floor(Math.random() * number);
-  }
-
-  //Bad logic here, need to fix
-  useEffect(() => {
-    if (gameType === "cpu") {
-      let randomIndex = getRandomInt(8);
-      changeBox(
-        box.map((prev) => {
-          if (prev.id === randomIndex) {
-            if (box[randomIndex].isClicked === false) {
-              if (mark === "O") {
-                setMark("X");
-                return {
-                  ...prev,
-                  mark: "O",
-                  isClicked: true,
-                };
-              }
+          })
+        );
+      } else if (mark === "O") {
+        getPlayerClickID(id);
+        changeBoard(
+          board.map((prev) => {
+            console.log("Player clicked at id of " + id);
+            if (prev.id === id && prev.isClicked === false) {
+              registerPlayerMove((prev) => (prev += 1));
+              setMark("X");
+              return { ...prev, mark: "O", isClicked: true };
             }
-          }
-          return prev;
-        })
-      );
+            return prev;
+          })
+        );
+      }
     }
-  }, [mark]);
+  }
+
+  let [emptyCellsArray, updateEmptyCellsObject] = useState<type[]>([]);
+  let randomNumbersArray: number[] = [];
+
+  useEffect(() => {
+    updateEmptyCellsObject(board.filter((prev) => prev.isClicked === false));
+  }, [board]);
+
+  function GetRandomIndex() {
+    for (let i = 0; i < emptyCellsArray.length; i++) {
+      randomNumbersArray.push(emptyCellsArray[i].id);
+    }
+    randomNumbersArray = randomNumbersArray.filter((item) => {
+      return item !== PlayerClickID;
+    });
+    return randomNumbersArray;
+  }
+
+  function getRandomNumber() {
+    GetRandomIndex();
+    return randomNumbersArray[
+      Math.floor(Math.random() * randomNumbersArray.length)
+    ];
+  }
+
+  //CPU move logic to listen for player move and respond
+  useEffect(() => {
+    randomNumber = getRandomNumber();
+    if (gameType === "cpu") {
+      if (mark === "O") {
+        console.log("Player move registered");
+        changeBoard(
+          board.map((prev) => {
+            console.log(
+              "prev.id " + prev.id + " Random number " + randomNumber
+            );
+            if (prev.id === randomNumber) {
+              console.log("CPU responded");
+              setMark("X");
+              return {
+                ...prev,
+                mark: "O",
+                isClicked: true,
+              };
+            }
+            return prev;
+          })
+        );
+      } else if (mark === "X") {
+        console.log("Player move registered");
+        changeBoard(
+          board.map((prev) => {
+            console.log(
+              "prev.id " + prev.id + " Random number " + randomNumber
+            );
+            if (prev.id === randomNumber) {
+              console.log("CPU responded");
+              setMark("O");
+              return {
+                ...prev,
+                mark: "X",
+                isClicked: true,
+              };
+            }
+            return prev;
+          })
+        );
+      }
+    }
+  }, [playerMove]);
 
   let [radioStatus, setRadioStatus] = useState(true);
 
@@ -331,6 +413,30 @@ function App() {
     <div className="flex flex-col w-full h-screen">
       <div className="w-[604px] h-auto mx-auto my-auto">
         <div className="pb-4">
+          <div className="flex flex-col">
+            <button
+              onClick={() => console.log(JSON.stringify(board, undefined, 2))}
+              className="text-red-500"
+            >
+              Test board state
+            </button>
+            <button
+              onClick={() =>
+                console.log(JSON.stringify(emptyCellsArray, undefined, 2))
+              }
+              className="text-red-500"
+            >
+              Test empty cells state
+            </button>
+            <button
+              onClick={() =>
+                console.log("---------------------------------------")
+              }
+              className="text-red-500"
+            >
+              Seperator
+            </button>
+          </div>
           <p>How you want to play?</p>
           <div className="flex items-center">
             <p className="pr-2">Solo</p>
@@ -345,14 +451,7 @@ function App() {
               onClick={() => setRadioStatus(true)}
             />
           </div>
-          <div>
-            <button
-              onClick={() => console.log(JSON.stringify(box, undefined, 2))}
-              className="text-red-500"
-            >
-              Test state
-            </button>
-          </div>
+
           <div className="flex items-center">
             <p className="pr-2">VS computer</p>
             <input
@@ -377,8 +476,8 @@ function App() {
           <div>
             <button
               onClick={() => {
-                changeBox(
-                  box.map((prev) => {
+                changeBoard(
+                  board.map((prev) => {
                     return { ...prev, mark: "", isClicked: false };
                   })
                 );
@@ -393,7 +492,7 @@ function App() {
           className="flex border-2 flex-wrap [&_div]:w-[200px]
     [&_div]:border-2 [&_div]:h-[200px] [&_div]:flex [&_div]:items-center [&_div]:justify-center"
         >
-          {box.map((item) => {
+          {board.map((item) => {
             return (
               <div onClick={() => handleClick(item.id)} key={String(item.id)}>
                 {item.isClicked === true ? (
